9f2fa735fb7e0234b3f889400f503d185d8a9809
YangZhuo1137165562 <1007510178@qq.com>
2020-02-03 19:06:08 +0800

    07a

---
+++ 一组/杨卓/2.3作业/yuque-前端/src/views/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home-page">
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.home-page{
+  width: 100%;
+  height: 100%;
+}
+</style>
