c337140d119f2e21bcd5101837dffce9a03f135f
liu <1623277211@qq.com>
2020-02-03 19:21:57 +0800

    第

---
+++ 一组/刘双2.3作业/qian/src/router/index.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+  {
+    path:"/",
+    component:()=>import('../views/Login/Login.vue')
+  },
+  {
+    path:"/registry",
+    component:()=>import('../views/registry/registry.vue')
+  },
+  {
+    path:"/home",
+    component:()=>import('../views/home/home.vue'),
+    children:[
+      {
+        path:"/file",
+        component:()=>import('../views/home/file/file.vue')
+      },
+      {
+        path:"/know",
+        component:()=>import('../views/home/know/know.vue')
+      }
+    ]
+  },
+  {
+    path:"/detial",
+    component:()=>import('../views/detial/detial.vue')
+  },
+  {
+    path:"/addknow",
+    component:()=>import('../views/addknow/addknow.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ 一组/刘双2.3作业/qian/src/views/addknow/addknow.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      新增知识库
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
