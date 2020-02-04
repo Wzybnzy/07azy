8c48192a431e76f2fc263593b3891236a36c1595
nn <592276614@qq.com>
2020-02-04 20:45:03 +0800

    yl

---
+++ 六组/郭勇良/2.4作业/yq-egg-example/app/controller/file/file.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class FileController extends Controller {
+  async echo() {
+    
+  }
+}
+
+module.exports = FileController;
---
+++ 六组/郭勇良/2.4作业/yq-egg-example/app/service/file/file.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class FileService extends Service {
+  
+}
+
+module.exports = FileService;
