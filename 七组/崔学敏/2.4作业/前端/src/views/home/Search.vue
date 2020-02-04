67b74ae7892e324115094d0b8478b5ecf6e8783f
CXM0601 <CXM0601>
2020-02-04 22:49:42 +0800

    2.4作业

---
+++ 七组/崔学敏/2.4作业/前端/src/views/home/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="search">
+      <input type="text" placeholder="请输入关键字"/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.search{
+  width:100%;
+  height:100%;
+  text-align: center;
+  input{
+    width:230px;
+    height:30px;
+    line-height: 30px;
+    appearance: none;
+  }
+}
+</style>
---
+++ 七组/崔学敏/2.4作业/前端/src/views/home/list/ZSK.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="zsk">
+<div class="top">
+  <div>名称</div>
+  <div>简介</div>
+  <div>操作</div>
+</div>
+    <div class="bottom" v-for="(item,index) in tableData" :key="index">
+      <div>{{item.title}}</div>
+      <div>{{item.content}}</div>
+     
+        <el-row>
+          <el-button type="primary">编辑</el-button>
+          <el-button type="danger">删除</el-button>
+        </el-row>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import httpAxios from "@/utils/request.js";
+export default {
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  methods: {
+    // tableRowClassName({ row, rowIndex }) {
+    //   if (rowIndex === 1) {
+    //     return "warning-row";
+    //   } else if (rowIndex === 3) {
+    //     return "success-row";
+    //   }
+    //   return "";
+    // }
+  },
+  async created() {
+    let res = await httpAxios.get("/api/lists");
+    console.log(res.data);
+    this.tableData = res.data.data;
+  }
+};
+</script>
+
+<style lang="scss">
+.zsk {
+  width: 100%;
+  height: 100%;
+  padding-top:100px;
+}
+.top{
+  width:100%;
+  height:50px;
+  line-height: 50px;
+  display: flex;
+  border-bottom: 1px solid #ccc;
+ 
+  div{
+    flex:1;
+    height:50px;
+    text-align: center;
+  }
+}
+
+.bottom{
+  width:100%;
+  height:50px;
+  line-height: 50px;
+  display: flex;
+  border-bottom: 1px solid #ccc;
+   div{
+    flex:1;
+    height:50px;
+    text-align: center;
+  }
+}
+</style>
