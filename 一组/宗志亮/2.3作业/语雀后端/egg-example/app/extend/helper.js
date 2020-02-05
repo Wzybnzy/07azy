6a0571f0cbd4f635d16530e107d3a8f0e7f88fe1
zong13921 <1832382882>
2020-02-06 00:14:05 +0800

    asdad

--- 一组/宗志亮/2.3作业/语雀后端/egg-example/app/extend/helper.js
+++
@@ -1,9 +0,0 @@
-const crypto =  require('crypto')
-const help = (pwd)=>{
-    return crypto.createHash('md5')
-    .update(pwd)
-    .digest('hex')
-}
-module.exports={
-    help
-}
--- 一组/宗志亮/2.3作业/语雀后端/yqegg/README.md
+++ 一组/宗志亮/2.3作业/语雀后端/yqegg/README.md
@@ -1,4 +1,4 @@
-# example
+# server
 
 
 
