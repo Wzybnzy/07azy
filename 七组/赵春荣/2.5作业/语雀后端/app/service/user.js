26c70901e4c59b297435bfbb21990d7e46e60c1f
zcr19970206 <1115670867@qq.com>
2020-02-05 22:22:06 +0800

    'serve'

---
+++ 七组/赵春荣/2.5作业/语雀后端/app/service/user.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class UserService extends Service {
+    async getuser(name) {
+        const $sql = `select * from yvqueuser where name='${name}'`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+    async registry(name, pwd) {
+        const $sql = `insert into yvqueuser (name,pwd) values ('${name}','${pwd}')`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+    async login(name, pwd) {
+        const $sql = `select * from yvqueuser where name='${name}' and pwd='${pwd}'`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+    async addknow(zisiku_name, zisiku_info, isshow, uid) {
+        const $sql = `insert into zhishiku (zisiku_name, zisiku_info, isshow, uid) values ('${zisiku_name}','${zisiku_info}','${isshow}','${uid}')`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+    async updateknow(zisiku_name, zisiku_info, isshow, uid, id) {
+        const $sql = ` update zhishiku  set zisiku_name='${zisiku_name}', zisiku_name='${zisiku_name}', isshow='${isshow}', uid='${uid}' where id='${id}'`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+    async delknow(id) {
+        const $sql = `delete from zhishiku where id='${id}'`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+    async getknow() {
+        const $sql = `select * from zhishiku`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+    async addfile(file_name, file_info, know_id, isshow, uid) {
+        const $sql = `insert into file (file_name, file_info, know_id, isshow, uid) values ('${file_name}','${file_info}','${know_id}','${isshow}','${uid}')`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+    async delfile(id) {
+        const $sql = `delete from file where id='${id}'`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+    async updatefile(file_name, file_info, know_id, isshow, uid, id) {
+        const $sql = ` update file  set file_name='${file_name}', file_info='${file_info}', know_id='${know_id}',isshow='${isshow}', uid='${uid}' where id='${id}'`;
+        const result = await this.app.mysql.query($sql);
+        return result;
+    }
+}
+
+module.exports = UserService;
