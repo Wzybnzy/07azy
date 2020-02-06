f2888fb9297bbb4d4261341272499be41079e743
zhangz <2212263788@qq.com>
2020-02-06 23:38:06 +0800

    ppp

---
+++ 六组/张卓/2_6作业/vue/src/views/home/search/search.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+    <div>
+        search.vue
+    </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+
+    };
+  },
+
+  computed: {},
+
+ // mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+
+</style>
