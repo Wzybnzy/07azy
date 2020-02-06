da877d7c966baa3ba97f233324dc399c59eec6dd
nn <592276614@qq.com>
2020-02-06 21:25:06 +0800

    gyl

---
+++ 六组/郭勇良/2.6作业/yq-egg/app/service/file/file.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class FileService extends Service {
+  async list() {
+    let sql=`select * from file`;
+    let res=await this.app.mysql.query(sql);
+    return res;
+  }
+  async add(obj){
+    let {filename, info, know_id,ischeck, uid}=obj;
+    let sql = `insert into file (filename, info,know_id, uid,ischeck) values ('${filename}','${info}','${know_id}','${uid}',${ischeck})`;
+    let res = await this.app.mysql.query(sql);
+    return res;
+  }
+  async del(id,uid){
+    let sql = `delete from file where id=${id} and uid=${uid}`;
+    let res = await this.app.mysql.query(sql);
+    return res;
+  }
+  async update(obj){
+    let {filename, info, id, uid}=obj;
+    let sql = `update file set filename='${filename}',info='${info}' where id=${id} and uid=${uid}`;
+    let res = await this.app.mysql.query(sql);
+    return res;
+  }
+}
+
+module.exports = FileService;
---
+++ 六组/郭勇良/2.6作业/yq-vue/src/views/index/list/list.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="list">
+      <div class="left">
+          <router-link v-for="(item,index) in list" :key="index" :to="item.path">{{item.title}}</router-link>
+      </div>
+      <div class="right">
+          <router-view></router-view>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+     data(){
+        return {
+            list:[{
+                path:"/index/list/file",
+                title:"文档"
+            },{
+                path:"/index/list/know",
+                title:"知识库"
+            },{
+                path:"/index/list/atten",
+                title:"关注"
+            },{
+                path:"/index/list/collect",
+                title:"收藏"
+            },{
+               path:"/index/list/person",
+               title:"个人信息"
+            }]
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .list{
+        width: 100%;
+        height: 100%;
+        display: flex;
+    }
+    .left{
+        flex: 2;
+      border-right:1px solid #ccc;
+      a{
+          display: block;
+          width: 100%;
+          height: 50px;
+          color: #000;
+          text-align: center;
+          line-height: 50px;
+          text-decoration: none;
+      }
+      .router-link-active{
+          background: orange;
+          color: #fff;
+      }
+    }
+    .right{
+        flex: 8;
+    }
+</style>
