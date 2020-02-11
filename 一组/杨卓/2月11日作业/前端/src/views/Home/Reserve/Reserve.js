40c5b88b007adba00ec87f0292224978e73e9eb2
YangZhuo1137165562 <1007510178@qq.com>
2020-02-11 22:23:38 +0800

    07a

---
+++ 一组/杨卓/2月11日作业/前端/src/views/Home/Reserve/Reserve.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Reserve extends Component {
+    render() {
+        return (
+            <div className='reserve-page'>
+                预定会议室
+            </div>
+        )
+    }
+}
