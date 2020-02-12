610438ecf5f9b55748a4ca3b95f581e9f69f10cf
zjm123zjm123 <3111502623@qq.com>
2020-02-12 09:31:52 +0800

    2.12的作业

---
+++ 一组/张贾敏2.12/reactquanxian/src/views/Login/Login.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import axios from "axios"
+export default class Login extends Component {
+    state={
+        name:"",
+        pwd:""
+    }
+
+    hander(e){
+        // console.log(e,"1111111111")
+        let name=e.target.name;
+        this.setState({
+            [name]:e.target.value
+        })
+    }
+
+  async handclick(){
+        let {name,pwd}=this.state;
+        console.log(name,pwd,"888888888")
+
+        let res=await axios.post("/login",{
+            name:name,
+            pwd:pwd
+        })
+        console.log(res,"444444444444444444444444444444")
+        if(res.data.code===1){
+            window.localStorage.token=res.data.token;
+            window.localStorage.name=res.data.name
+            this.props.history.push("/home/reserve")
+        }else if(res.data.code===0){
+            alert("登录失败")
+        }else if(res.data.code===2){
+            alert("用户名不存在，请重新注册")
+        }else if(res.data.code===3){
+            alert("参数不能为空")
+        }else if(res.data.code===4){
+            alert("参数类型校验失败")
+        }
+    }
+
+    resclick(){
+        this.props.history.push("/registry")
+    }
+    render() {
+        return (
+            <div>
+               <input type="text" placeholder="请输入用户名" value={this.state.name} name="name" onChange={this.hander.bind(this)}/>
+               <input type="text" placeholder="请输入密码" value={this.state.pwd} name="pwd" onChange={this.hander.bind(this)}/>
+               <button onClick={this.handclick.bind(this)}>登录</button>
+               <button onClick={this.resclick.bind(this)}>注册</button>
+            </div>
+        )
+    }
+}
