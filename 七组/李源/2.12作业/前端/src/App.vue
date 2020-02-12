9e2a78925af32bc124a32c238e832c922ece99c1
2452024537 <2452024537@qq.com>
2020-02-12 22:34:26 +0800

    ''

---
+++ 七组/李源/2.12作业/前端/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+       <item></item>
+  </div>
+</template>
+
+<script>
+import item from './components/item';
+export default {
+  name: 'app',
+  components:{
+      item
+  }
+}
+</script>
+
+<style>
+*{
+  margin:0;
+  padding:0;
+  list-style:none;
+}
+.app{
+  width:100%;
+  height:100%;
+  display: flex;
+  flex-direction: column;
+  font-size: 13px;
+}
+</style>
---
+++ 七组/李源/2.12作业/前端/src/views/registry/registry.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="login">
+       <div class = 'headl'>注册</div>
+       <div class = 'sectl'>
+            <li>账户:<input type="text" v-model = 'name'></li>
+            <li>密码:<input type="text" v-model = 'pwd'></li>
+            <span class = 'spanl' @click = 'registry'>注册</span>
+       </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default{
+  name: 'login',
+  data(){
+      return{
+         user:'',
+         pass:''
+      }
+  },
+  created(){
+     
+  },
+  methods:{
+      async registry(){
+            let {name,pwd}=this;
+            let res=await axios.post("/api/registry",{name,pwd});
+            if(res.data.code==1){
+                this.$router.push("/login")
+            }else if(res.data.code==2){
+                console.log(res.data)
+            }
+        }
+  }
+}
+</script>
+
+<style>
+*{
+  margin:0;
+  padding:0;
+  list-style:none;
+}
+.login{
+  width:100%;
+  height:100%;
+  display: flex;
+  flex-direction: column;
+  font-size: 13px;
+}
+.headl{
+  width:100%;
+  height:40px;
+  line-height: 40px;
+  text-align: center;
+  background: pink;
+}
+.sectl{
+  width:100%;
+  height:auto;
+  margin-top: 30px;
+}
+.sectl li{
+  width:100%;
+  height:60px;
+  padding-left: 10px;
+  display: flex;
+  align-items: center;
+}
+.sectl li input{
+  display: inline-block;
+  width:80%;
+  height:27px;
+  border:solid 1px #f00;
+  margin-left: 10px;
+}
+.spanl{
+  display: inline-block;
+  width:60px;
+  height:25px;
+  background:pink;
+  line-height: 25px;
+  text-align: center;
+  position: fixed;
+  top: 220px;
+  left:40%;
+}
+</style>
