92546a3ab4d4976e2bb77d1d9829c88b97fa245a
zhangz <2212263788@qq.com>
2020-02-12 23:43:51 +0800

    zz212

---
+++ 六组/张卓/2_12作业/rout/src/views/registry.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class registry extends Component {
+    render() {
+        return (
+            <div>
+                注册 
+            </div>
+        )
+    }
+}
