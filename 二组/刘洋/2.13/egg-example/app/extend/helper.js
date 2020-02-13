6a2cba173b3ccaba283d83c2bf171d65af947aaf
ly <1971005712@qq.com>
2020-02-13 22:27:55 +0800

    2.13

---
+++ 二组/刘洋/2.13/egg-example/app/extend/helper.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto');
+
+const encryption=(pwd)=>{
+    return crypto.createHash("md5")
+                 .update(pwd)
+                 .digest("hex")
+}
+
+module.exports={
+    encryption
+}
